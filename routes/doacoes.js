33b6dbceff52ef304e0f3a5bf5d6424dbbea08a1
Monni-Tonon <monnicy@gmail.com>
2023-04-21 14:16:16 -0300

    criacao da rota determinando valor total doado por ong

--- routes/doacoes.js
+++ routes/doacoes.js
@@ -1,11 +1,36 @@
+const sequelize = require("../database/db");
 const Doacao = require("../models/Doacao");
-const express = require("express");
 const Ong = require("../models/Ong");
+const express = require("express");
 const doacoes = express.Router();
 
 doacoes.route("/")
 .get(async (req, res) => {
-    res.json({mensagem: "rota doacoes get"})
+    const {ongId} = req.query;
+    
+    try {
+        if (ongId) {
+            const findOng = await Ong.findOne({where: {id: ongId}})
+            
+            if(!findOng) {
+                return res.status(404).json({message: `ONG inexistente.`})
+            }
+            const total = await Doacao.findOne({
+                attributes: [
+                    [sequelize.fn("SUM", sequelize.col("valor")), "valor"]    
+                ],
+                where: { ongId }
+            });
+
+            res.status(200).json({message: `A instituição ${findOng.nome} recebeu um total de R$ ${total.valor} em doações.`});
+        } else {
+            const doacoes = await Doacao.findAll();
+            res.status(200).json(doacoes);
+        }
+        
+    } catch(err) {
+        return res.status(500).json(err);
+    }
 })
 
 .post(async (req, res) => {
